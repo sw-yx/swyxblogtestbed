35c5dbbd460253451945fbb4bda8d8280b143c99
sw-yx <swyx@swyx.io>
2017-12-21 11:10:27 +0800

    add admin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,23 @@
 module.exports = {
+  pathPrefix: "/swyxblogtestbed",
   siteMetadata: {
     title: `Gatsby Default Starter`
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-react-helmet",
     `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/src/pages`,
-        name: 'pages'
+        name: "pages"
       }
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/src/img`,
-        name: 'images'
+        name: "images"
       }
     },
     `gatsby-plugin-sharp`,
